abab1c67311f6e5957ed15dcbb12c293baf36831
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-12 20:58:14 -0800

    improved compare function to keep track of letters that were already checked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var words = ["this", "that", "bicep",
  "drain", "your", "avicii", "china"];
 var wins = 0;
 var guesses = 12;
+var usedLetters = [];
 var randomWord = words[Math.floor(Math.random()*words.length)];
 console.log(randomWord);
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -15,19 +16,30 @@ function wordLength (){
 
 //compare the letters in the chosen word with user guesses
 function compare (event){
+  isFound = false;
+  var userVal = event.which || event.keyCode;
   for(var i = 0; i < randomWord.length; i++){
-    var userVal = event.which || event.keyCode;
     var randLetter = randomWord.charCodeAt(i);
-    if(userVal === randLetter){
+    console.log(usedLetters.indexOf(String.fromCharCode(userVal)));
+    if(userVal === randLetter &&
+      usedLetters.indexOf(String.fromCharCode(userVal)) < 0)
+    {
       document.getElementById("correct").innerHTML = "Correct "
       + randomWord[i];
+      isFound = true;
+      usedLetters.push(String.fromCharCode(userVal))
       break;
-    } else {
-      console.log(guesses);
-      guesses--;
-      document.getElementById("incorrect").innerHTML = "Incorrect "
-      + guesses;
+    } else if (usedLetters.indexOf(String.fromCharCode(userVal)) >= 0) {
+      document.getElementById("used").innerHTML = "Already used dumby "
+      + randomWord[i];
       break;
     }
+  }
+  if(!isFound){
+    guesses--;
+    document.getElementById("incorrect").innerHTML = "Incorrect "
+    + guesses;
+    if (usedLetters.indexOf(String.fromCharCode(userVal)) < 0)
+      usedLetters.push(String.fromCharCode(userVal));
   }
 }
