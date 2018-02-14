e3ddef918f4dd45a585739c92055e578fd404f26
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-14 10:57:13 -0800

    made it so that the hangman game can be played more than once without refreshing the page still needs work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,72 @@
 //Initial variables
-var words = ["avicii", "tiesto", "swedish house mafia", "drain"];
+var words = ["avicii", "tiesto", "swedish", "drain"];
 var wins = 0;
 var guesses = 12;
 var usedLetters = [];
 var randomWord = words[Math.floor(Math.random()*words.length)];
-const answer = [];
+var winCounter = randomWord.length;
+var answer = [];
 console.log(randomWord);
 setArray();
 
-function usedLetters(array) {
-
-}
 
 //Function to set answer array to false
-function setArray()
-{
+function setArray(){
+  usedLetters = []
+  answer = []
   for (var i = 0; i < randomWord.length; i++) {
     answer[i] = false;
     print(answer);
   }
 }
 //compare the letters in the chosen word with user guesses
-document.onkeyup = function(event)
-{
+document.onkeyup = function(event){
+
   isFound = false;
   var userVal = event.key;
   console.log(userVal);
 
-  for(var i = 0; i < randomWord.length; i++)
-  {
+  for(var i = 0; i < randomWord.length; i++){
     var randLetter = randomWord[i].toLowerCase();
     if(userVal === randLetter &&
-      usedLetters.indexOf(userVal) < 0)
-    {
+      usedLetters.indexOf(userVal) < 0){
       document.getElementById("correct").innerHTML = "Correct "
       + randomWord[i];
       isFound = true;
       for (var i = 0; i < answer.length; i++) {
         if(userVal === randomWord[i] && answer[i] !== true){
           answer[i] = true;
+          winCounter--;
         }
       }
       print(answer);
       continue;
     }
-    else if (usedLetters.indexOf(userVal) >= 0)
-    {
+    else if (usedLetters.indexOf(userVal) >= 0){
       break;
     }
   }
-  if(!isFound)
-  {
+
+  if(!isFound){
     guesses--;
     document.getElementById("incorrect").innerHTML = "Incorrect "
     + guesses;
   }
+
   if (usedLetters.indexOf(userVal) < 0)
     usedLetters.push(userVal);
-    document.getElementById("used").innerHTML = " ";
-    for (var i = 0; i < usedLetters.length; i++) {
-      document.getElementById("used").innerHTML += usedLetters[i] + " ";
-    }
+    used_Letters(usedLetters);
+
+  if(winCounter === 0){
+    wins++;
+    randomWord = words[Math.floor(Math.random()*words.length)];
+    winCounter = randomWord.length;
+    console.log(randomWord);
+    setArray();
+  }
 }
 
-function print (array)
-{
+function print (array){
   document.getElementById("print").innerHTML = "";
   for (var i = 0; i < array.length; i++) {
     if(answer[i] === true){
@@ -72,5 +74,12 @@ function print (array)
     } else{
       document.getElementById("print").innerHTML += "_ " + " ";
     }
+  }
+}
+
+function used_Letters(array) {
+  document.getElementById("used").innerHTML = " ";
+  for (var i = 0; i < usedLetters.length; i++) {
+    document.getElementById("used").innerHTML += usedLetters[i] + " ";
   }
 }
